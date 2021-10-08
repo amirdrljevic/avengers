077f710114221f8faedad048d4391abab524894a
amirdrljevic <amirdrljevic@gmail.com>
2021-10-08 22:57:27 +0200

    Pagination on post index page implemented (#5)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,9 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    #@posts = Post.all
+    #@posts = Post.paginate(:page => params[:page], :per_page => 10)
+    @posts = Post.paginate(page: params[:page], per_page: 2)
     #@posts = current_user.posts.load if user_signed_in?
   end
 
